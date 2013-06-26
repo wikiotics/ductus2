366747eb405643a3eaa9b3b247a869e712c52641
Jim Garrison <garrison@wikiotics.org>
2013-06-26 02:11:14 -0700

    enabled clickjack prevention middleware

--- ductus2/settings.py
+++ ductus2/settings.py
@@ -99,7 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'ductus2.urls'
