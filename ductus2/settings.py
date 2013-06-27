912458a1e361aad620d79cabc9fba0ac5619c3b7
Jim Garrison <garrison@wikiotics.org>
2013-06-26 23:35:08 -0700

    enable django-debug-toolbar, using the fork the works for python3

--- ductus2/settings.py
+++ ductus2/settings.py
@@ -93,6 +93,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django_extensions',
+    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
