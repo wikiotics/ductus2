94675b4fed6ba9454fd89b5972bfa5cdc70eae97
Laurent Savaete <laurent.savaete@gmail.com>
2013-07-04 00:14:44 +0200

    setup django rest framework

--- ductus2/settings.py
+++ ductus2/settings.py
@@ -141,7 +141,13 @@ INSTALLED_APPS = (
     'ductus2.wiki',
     'ductus2.textpages',
     'ductus2.podcasts',
+    'rest_framework',
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
+}
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
