d033a3e3539f64391434090884c690b5b338111e
Laurent Savaete <laurent.savaete@gmail.com>
2013-07-04 12:14:08 +0200

    tweak api output so we can get just the info we need

--- ductus2/podcasts/serializers.py
+++ ductus2/podcasts/serializers.py
@@ -2,13 +2,44 @@ from ductus2.podcasts.models import PodcastPage, PodcastRevision
 from rest_framework import serializers
 
 class PodcastRevisionSerializer(serializers.HyperlinkedModelSerializer):
+    """Give only a summary of a revision, useful for listings"""
+
+    class Meta:
+        model = PodcastRevision
+        fields = ('id', 'timestamp', 'podcast', 'author_ip', 'title')
+
+class PodcastLatestRevisionSerializer(serializers.HyperlinkedModelSerializer):
+    """Give full details of the latest revision"""
 
     class Meta:
         model = PodcastRevision
         fields = ('id', 'timestamp', 'podcast', 'author_ip', 'title', 'description', 'rows')
 
 class PodcastPageSerializer(serializers.ModelSerializer):
+    """The default serializer, used for saving podcasts"""
 
+    class Meta:
+        model = PodcastPage
+        fields = ()
+
+class PodcastPageDetailSerializer(serializers.ModelSerializer):
+    """Serialize only the latest revision of a podcast, throw in full details."""
+
+    revision = serializers.SerializerMethodField('get_latest_revision')
+
+    class Meta:
+        model = PodcastPage
+        fields = ()
+
+    def get_latest_revision(self, obj):
+        rev = obj.get_latest_revision()
+        serializer = PodcastLatestRevisionSerializer(rev)
+        return serializer.data
+
+class PodcastPageHistorySerializer(serializers.ModelSerializer):
+    """Serialize a podcast with its history of revisions"""
+
+    revisions = PodcastRevisionSerializer(source='revisions', required=False)
     class Meta:
         model = PodcastPage
         fields = ()
--- ductus2/podcasts/views.py
+++ ductus2/podcasts/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from ductus2.podcasts.models import PodcastPage, PodcastRevision
-from rest_framework import viewsets
-from ductus2.podcasts.serializers import PodcastPageSerializer, PodcastRevisionSerializer
+from rest_framework import viewsets, response
+from ductus2.podcasts.serializers import PodcastPageSerializer, PodcastRevisionSerializer, PodcastPageDetailSerializer, PodcastPageHistorySerializer
 
 class PodcastPageViewSet(viewsets.ModelViewSet):
     """
@@ -9,9 +11,27 @@ class PodcastPageViewSet(viewsets.ModelViewSet):
     queryset = PodcastPage.objects.all()
     serializer_class = PodcastPageSerializer
 
+    def retrieve(self, request, pk=None):
+        """ GET /api/podcasts/<id>/ returns all fields in the podcast, for the latest revision.
+            GET /api/podcasts/<id>/?history returns only limited info, but for all revisions of a podcast.
+            """
+        try:
+            podcast = PodcastPage.objects.get(pk=pk)
+        except PodcastPage.DoesNotExist:
+            raise Http404
+
+        if 'history' in request.QUERY_PARAMS:
+            s = PodcastPageHistorySerializer
+        else:
+            s = PodcastPageDetailSerializer
+
+        serializer = s(podcast, context={'request': request})
+        return response.Response(serializer.data)
+
 class PodcastRevisionViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows revisions to be viewed or edited.
+    This should normally not be used much.
     """
     queryset = PodcastRevision.objects.all()
     serializer_class = PodcastRevisionSerializer
