ea1a211185192a3fb375a377e5ae2d72d10c2545
Laurent Savaete <laurent.savaete@gmail.com>
2013-07-05 13:26:52 +0200

    enable django admin so we can login for now

--- ductus2/urls.py
+++ ductus2/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 from ductus2.podcasts import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 router = routers.DefaultRouter()
 router.register(r'podcasts', views.PodcastPageViewSet)
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
